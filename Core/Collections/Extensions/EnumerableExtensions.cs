659a7d646b47cd48896ec414b4455e00a3033ba8
sheng5521627 <lpw-157@163.com>
2017-03-07 19:58:19 +0800

    201703071958

---
+++ Core/Collections/Extensions/EnumerableExtensions.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Collections.Extensions
+{
+    /// <summary>
+    /// Enumerable扩展
+    /// </summary>
+    public static class EnumerableExtensions
+    {
+        /// <summary>
+        /// 用指定的分隔符链接字符串集合
+        /// </summary>
+        /// <param name="source"></param>
+        /// <param name="separator"></param>
+        /// <returns></returns>
+        public static string JoinAsString(this IEnumerable<string> source, string separator)
+        {
+            return string.Join(separator, source);
+        }
+
+        /// <summary>
+        /// 用指定的分隔符链接字符串集合
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="source"></param>
+        /// <param name="separator"></param>
+        /// <returns></returns>
+        public static string JoinAsString<T>(this IEnumerable<T> source, string separator)
+        {
+            return string.Join(separator, source);
+        }
+
+
+        /// <summary>
+        /// 根据条件筛选集合
+        /// </summary>
+        /// <typeparam name="T">集合数据类型</typeparam>
+        /// <param name="source"></param>
+        /// <param name="condition"></param>
+        /// <param name="predicate"></param>
+        /// <returns></returns>
+        public static IEnumerable<T> WhereIf<T>(this IEnumerable<T> source, bool condition, Func<T, bool> predicate)
+        {
+            return condition ? source.Where(predicate) : source;
+        }
+
+        /// <summary>
+        /// 根据条件筛选集合
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="source"></param>
+        /// <param name="condition"></param>
+        /// <param name="predicate"></param>
+        /// <returns></returns>
+        public static IEnumerable<T> WhereIf<T>(this IEnumerable<T> source, bool condition, Func<T, int, bool> predicate)
+        {
+            return condition ? source.Where(predicate) : source;
+        }
+    }
+}
---
+++ CoreTest/CollectionExtensionsTest.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Core.Collections.Extensions;
+
+namespace CoreTest
+{
+    [TestClass]
+    public class CollectionExtensionsTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            ICollection<int> list = new List<int>();
+            list.Add(1);
+
+            Assert.IsTrue(list.IsNullOrEmpty());
+        }
+    }
+}
